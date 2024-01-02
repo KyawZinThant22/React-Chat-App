1caa8b3abd4d599c81b8a7c5c22162a7dc727507
Kyaw Zin Thant <kyawthantver2@gmail.com>
2024-01-03 01:43:35 +0630

    new folder structure

---
+++ server/frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import ChatPage from "./pages/ChatPage";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/chat/:username" element={<ChatPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
